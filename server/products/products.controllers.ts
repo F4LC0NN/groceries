84f39cde3aba10a760429fcc03061e843dd3188c
f4lc0nn <thomasruben25@gmail.com>
2023-05-17 07:00:11 +0200

    Initialize database & create 1st API endpoint

--- server/products/products.controllers.ts
+++ server/products/products.controllers.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from "express";
 
+import prisma from "../script";
+
 async function getAllProducts(req: Request, res: Response) {
-    res.json({ message: "Under construction..." });
+    let data = null;
+
+    try {
+        data = await prisma.product.findMany();
+    } catch (error) {
+        console.error(error);
+        res.json({ error: error });
+    }
+
+    if (!data || data == null || data.length <= 0)
+        return res.status(404).json({ info: "No products found", error: 404 });
+
+    res.json(data);
 }
 
 export { getAllProducts };
