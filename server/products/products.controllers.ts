04923c1724517c48f000b21a832164deb0e5cb75
f4lc0nn <thomasruben25@gmail.com>
2023-05-17 14:31:14 +0200

    Update code to reflect wrong req body & data

--- server/products/products.controllers.ts
+++ server/products/products.controllers.ts
@@ -35,7 +35,12 @@ async function addProduct(req: Request, res: Response) {
     }
 
     if (!data || data === null || Object.keys(req.body).length === 0)
-        return res.status(404).json({ info: "No products found", error: 404 });
+        return res
+            .status(400)
+            .json({
+                info: "Not enough information to add a product",
+                error: 400,
+            });
 
     res.status(201).json(data);
 }
