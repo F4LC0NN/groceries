30f390b18d4c71442df7cfecae1d50b42d09eecf
f4lc0nn <thomasruben25@gmail.com>
2023-05-17 13:53:21 +0200

    Wrap API provider to App to allow global data

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ApiProvider from "@providers/ApiProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ApiProvider>
+            <App />
+        </ApiProvider>
     </React.StrictMode>
 );
