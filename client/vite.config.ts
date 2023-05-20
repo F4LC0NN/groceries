a59af8d417a89d18b00d1ac465fee7d7c17d8414
f4lc0nn <thomasruben25@gmail.com>
2023-05-20 09:49:20 +0200

    Fix host/port vite issue after docker build/run

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
     plugins: [react(), tsConfigPaths()],
     server: {
-        host: true,
+        host: "0.0.0.0",
         port: 3000,
     },
 });
